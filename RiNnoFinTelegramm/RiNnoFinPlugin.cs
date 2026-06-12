e594edacb02b355d52a369fcfb44183fb09cbaed
killerronnym <undhbt@gmail.com>
2026-06-12 15:30:58 +0200

    Hotfix to 1.0.4.53: Fix frontend CACHE bug preventing new JS from loading

--- RiNnoFinTelegramm/RiNnoFinPlugin.cs
+++ RiNnoFinTelegramm/RiNnoFinPlugin.cs
@@ -67,7 +67,7 @@ public class RiNnoFinPlugin : BasePlugin<PluginConfiguration>, IPlugin, IHasWebP
                 EnableInMainMenu = true,
                 MenuIcon = "send"
             },
-            new PluginPageInfo { Name = "RiNnoFinTelegramm_v10439.js", EmbeddedResourcePath = $"{typeof(RiNnoFinPlugin).Namespace}.Assets.Config.config.js" },
+            new PluginPageInfo { Name = "RiNnoFinTelegramm_v10453.js", EmbeddedResourcePath = $"{typeof(RiNnoFinPlugin).Namespace}.Assets.Config.config.js" },
             new PluginPageInfo { Name = "RiNnoFinTelegramm.css", EmbeddedResourcePath = $"{typeof(RiNnoFinPlugin).Namespace}.Assets.Config.config.css" }
         ];
     }
